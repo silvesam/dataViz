6fa937a941c80c1c104e1c9889a0b995362ef6b6
Samantha Silveira <silvesam@umich.edu>
2017-12-08 12:01:18 -0500

    timeslider working, exclude year 2013 (2013 gives an error for some reason, so I stop at 2012)

--- map.js
+++ map.js
@@ -4,15 +4,18 @@ $(document).ready(function(){
         .defer(d3.json, '/d3-geomap/topojson/world/countries.json')
         .await(ready);
 
-        yearSlider = '2012';
+        yearSlider = 'Year:2012';
 
         $("#timeslide").on("input", function() {
 
     		$('#range').html('Year: '+$(this).val());
 		});
 		$("#timeslide").on("mouseup", function() {
-			yearSlider = $(this).val();
+			yearSlider = 'Year:'+$(this).val();
 			console.log('yearslider val: ', yearSlider)
+			d3.select('#map').html(null);
+			var mapUpdate = return_map(yearSlider);
+			draw_map(mapUpdate);
     		
 		});
 })
@@ -20,51 +23,54 @@ $(document).ready(function(){
 
 function ready(error, countries){
 
+	// var format = function(d){
+	// 	return Math.round(d) + ' yrs';
+	// }
+
+	//something about Year:2013 makes it fail
+	// var map = d3.geomap.choropleth()
+	//     .geofile('/d3-geomap/topojson/world/countries.json')
+	//     .colors(colorbrewer.YlGnBu[9])
+	//     .column('Year:2012')
+	//     .domain([40, 100])
+	//     .legend(true)
+	//     .format(format)
+	//     .unitId('CountryCode');
+
+	var map = return_map(yearSlider);
+	draw_map(map);
+
+	// d3.csv('/data/life_expectancy_parsed_excel.csv', function(error, data) {
+	//     d3.select('#map')
+	//         .datum(data)
+	//         .call(map.draw, map);
+	// });
+}
+
+function return_map(column_name){
 	var format = function(d){
 		return Math.round(d) + ' yrs';
 	}
-
-	//something about Year:2013 makes it fail
 	var map = d3.geomap.choropleth()
 	    .geofile('/d3-geomap/topojson/world/countries.json')
 	    .colors(colorbrewer.YlGnBu[9])
-	    .column('Year:2012')
+	    .column(column_name)
 	    .domain([40, 100])
 	    .legend(true)
 	    .format(format)
 	    .unitId('CountryCode');
 
+	return map;
+}
+
+function draw_map(map){
 	d3.csv('/data/life_expectancy_parsed_excel.csv', function(error, data) {
+		//map = return_map(yearSlider);
 	    d3.select('#map')
 	        .datum(data)
 	        .call(map.draw, map);
 	});
 }
-
-// function return_map(column_name){
-// 	var format = function(d){
-// 		return Math.round(d) + ' yrs';
-// 	}
-// 	var map = d3.geomap.choropleth()
-// 	    .geofile('/d3-geomap/topojson/world/countries.json')
-// 	    .colors(colorbrewer.YlGnBu[9])
-// 	    .column(column_name)
-// 	    .domain([40, 100])
-// 	    .legend(true)
-// 	    .format(format)
-// 	    .unitId('CountryCode');
-
-// 	return map;
-// }
-
-// function draw_map(){
-// 	d3.csv('/data/life_expectancy_parsed.csv', function(error, data) {
-// 		map = return_map(yearSlider);
-// 	    d3.select('#map')
-// 	        .datum(data)
-// 	        .call(map.draw, map);
-// 	});
-// }
 
 
 //using map library
